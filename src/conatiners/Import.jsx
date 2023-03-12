c29d555724202f7d18909379011fec103fbf164b
muskan2532150 <rg2532150@gmail.com>
2023-03-13 03:10:35 +0530

    Export all container file from a single file

---
+++ src/conatiners/Import.jsx
@@ -0,0 +1,7 @@
+export {default as Blog} from './Blog';
+export {default as Features} from './Features';
+export {default as Header} from './Header';
+export {default as Intro} from './Intro';
+export {default as Footer} from './Footer';
+
+
