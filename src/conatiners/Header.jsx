5c499a8dfe6b9ca3f5aac78f87ae250296ee2cc2
muskan2532150 <rg2532150@gmail.com>
2023-03-14 04:10:35 +0530

    Update header section

--- src/conatiners/Header.jsx
+++ src/conatiners/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import '../assets/Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className='header'>
+<div className='intro'>
+  <h1 className="heading">
+  Let’s Build Something
+amazing with GPT-3
+OpenAI
+  </h1>
+  <p>
+  Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. 
+  Indulgence way everything joy alteration boisterous the attachment. 
+  Party we years to order allow asked of.
+  </p>
+<div className="intro-form">
+  <input type="email" placeholder='YOUR EMAIL ADDRESS'/>
+  <button className="intro-form-btn" >Get Started</button>
+</div>
+    </div>
+    <div className="intro-img">
+      <img src="images/intro-img.png" alt="" />
+    </div>
+    </section>
   )
 }
 
