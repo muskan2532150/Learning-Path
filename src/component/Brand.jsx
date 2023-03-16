bc372707261b3b64694a8e6c2f069395e2a85bf0
muskan2532150 <rg2532150@gmail.com>
2023-03-16 12:20:35 +0530

    Update Brand section

--- src/component/Brand.jsx
+++ src/component/Brand.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../assets/Brand.css'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='brand'>
+      <ul>
+        <li><img src="images/google.png" alt="google" /></li>
+        <li><img src="images/slack.png" alt="slack" /></li>
+        <li><img src="images/dropbox.png" alt="dropbox" /></li>
+        <li><img src="images/shopify.png" alt="shopify" /></li>
+      </ul>
+    </div>
   )
 }
 
