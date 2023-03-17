bdd9094381720064ce2ba021d24d932a32049940
muskan2532150 <rg2532150@gmail.com>
2023-03-18 01:55:35 +0530

    Update Intro code

--- src/conatiners/Intro.jsx
+++ src/conatiners/Intro.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import '../assets/Intro.css'
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <section className='gpt-intro' id='#gpt-intro'>
+      <div className="sec-one">
+        <h4>What is GPT-4</h4>
+        <p>
+          We so opinion friends me message as delight. Whole front do of plate heard oh ought.
+          His defective nor convinced residence own. Connection has put impossible own apartments boisterous.
+          At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.
+        </p>
+      </div>
+      <div className="sec-two">
+        <p>The possibilities are beyond your imagination</p>
+        <a href="#">Explore The Library</a>
+      </div>
+      <div className="sec-three">
+        <div><h6>Chatbots</h6>
+          <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
+          </div>
+        <div><h6>Knowledgebase</h6>
+          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p></div>
+        <div><h6> Education</h6>
+          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p></div>
+      </div>
+      <div className="sec-three">
+
+      </div>
+
+    </section>
   )
 }
 
