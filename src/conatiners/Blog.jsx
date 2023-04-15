5d2f0a27467e20eb01efc89069c7250fb4cbf2d0
muskan2532150 <rg2532150@gmail.com>
2023-04-16 01:47:35 +0530

    Update Blog file code

--- src/conatiners/Blog.jsx
+++ src/conatiners/Blog.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Article from '../component/Article'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section>
+      <div>
+        <h4 className='blog-header'> A lot is happening,We are blogging about it.</h4>
+      </div>
+      <Article/>
+    </section>
   )
 }
 
