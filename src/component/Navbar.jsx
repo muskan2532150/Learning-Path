a2f9bab72532e80bb734d8b7c6b8a81cfa661e7c
muskan2532150 <rg2532150@gmail.com>
2023-04-19 01:59:35 +0530

    Add Navbar burger function

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import '../assets/Navbar.css'
 
 const Navbar = () => {
+  const [toggle, setToggle ] = useState(false);
+
+  const MenuHandler = () =>{
+   setToggle(!toggle);
+  }
+
+  const closeMenuHandler = () =>{
+    document.querySelector('.nav-li').display="none";
+    document.querySelector(".sign-bar-menu").display="none";
+    setToggle(!toggle);
+   }
+ 
+
   return (
     <div className='nav'>
       <h1 className="logo">GPT-3</h1>
-      <ul className='nav-li'>
-        <li>Home</li>
-        <li>What is GPT?</li>
-        <li>Open AI</li>
-        <li>Case Studies</li>
-        <li>Library</li>
-      </ul>
-    <div className="sign-bar">
-      <a href="#">Sign in</a>
-      <button className="sign-btn">Sign Up</button>
-    </div>
+      <div className='nav-container'>
+        <ul className='nav-li d-flex'>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#gpt-intro">What is GPT3?</a></li>
+          <li><a href="#possibility">Open AI</a></li>
+          <li><a href="#features">Case Studies</a></li>
+          <li><a href="#blog">Library</a></li>
+        </ul>
+      </div>
+      <div className="sign-bar">
+        <a href="#">Sign in</a>
+        <button className="sign-btn">Sign Up</button>
+      </div>
+      <div className={`sidenavbar ${toggle ? 'd-flex': 'd-none'} `}>
+      <ul className='sidenav-li d-flex'>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#gpt-intro">What is GPT3?</a></li>
+          <li><a href="#possibility">Open AI</a></li>
+          <li><a href="#features">Case Studies</a></li>
+          <li><a href="#blog">Library</a></li>
+          <div className="sign-bar-menu">
+          <a href="#">Sign in</a>
+          <button className="sign-btn">Sign Up</button>
+        </div>
+        </ul>
+        </div>
+      <div>
+         <button className='icon' onClick={MenuHandler}>
+       {!toggle?<button className='icon'><RiMenu3Line/></button>:
+       <button className='icon' onClick={closeMenuHandler}><RiCloseLine/></button>} </button>
+      </div>
     </div>
   )
 }
