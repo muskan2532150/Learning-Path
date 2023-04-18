f4c8421329547218223109a1a02c62071c80d464
muskan2532150 <rg2532150@gmail.com>
2023-04-18 12:59:35 +0530

    Add Footer code

--- src/conatiners/Footer.jsx
+++ src/conatiners/Footer.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import '../assets/Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+      <div className="footer">
+        <div className="footer-heading">
+          <h1 className="footer-heading-h1">Do you want to step in to the future before others</h1>
+        </div>
+
+        <div className="footer-btn">
+          <p>Request Early Access</p>
+        </div>
+
+        <div className="footer-links">
+          <div className="footer-links_logo">
+            <img src='images/logo.svg' alt="logo" />
+            <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+          </div>
+          <div className="footer-links_div">
+            <h4>Links</h4>
+            <p>Overons</p>
+            <p>Social Media</p>
+            <p>Counters</p>
+            <p>Contact</p>
+          </div>
+          <div className="footer-links_div">
+            <h4>Company</h4>
+            <p>Terms & Conditions </p>
+            <p>Privacy Policy</p>
+            <p>Contact</p>
+          </div>
+          <div className="footer-links_div">
+            <h4>Get in touch</h4>
+            <p>Crechterwoord K12 182 DK Alknjkcb</p>
+            <p>085-132567</p>
+            <p>info@payme.net</p>
+          </div>
+        </div>
+
+        <div className="footer-copyright">
+          <p>@2021 GPT-3. All rights reserved.</p>
+        </div>
+      </div>
   )
 }
 
