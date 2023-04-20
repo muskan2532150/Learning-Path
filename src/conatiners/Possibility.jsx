906aa23e3b36c7c15d27eb34e87c0a5ac0479533
muskan2532150 <rg2532150@gmail.com>
2023-04-20 05:59:35 +0530

    Add Possibility code and css file

--- src/conatiners/Possibility.jsx
+++ src/conatiners/Possibility.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import '../assets/Possibility.css'
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className="possibility d-flex">
+    <div className="feature-heading w-60">
+      <img src="../images/Feature-image.png" alt=" " className="feature-img" />
+    </div>
+    <div className='feature-2 possibility-body d-flex'>
+      <p className='possibility-body-p1 '>Request Early Access to Get Started</p>
+      <p className='possibility-body-p2 text-color'>Request Early Access to Get Started</p>
+      <p className='feature-body'>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <p className='possibility-body-p4'>Request Early Access to Get Started</p>
+    </div>
+
+  </div>
   )
 }
 
